5ad5e524bc6c5c5ac0f63537dda53e559432dff8
Andrew Schell <paintstone@gmail.com>
2025-02-20 12:46:19 -0800

    updating test_orm.py, added black connect

--- orm.py
+++ orm.py
@@ -1,17 +1,15 @@
-from sqlalchemy import Columnn ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import mapper, relationship, MetaData
 
-Base = declarative_base()
+import model
 
-class Order(Base):
-	id = Columnn(Integer, primary_key=True)
+metadata = MetaData()
 
-class OrderLine(Base):
-	id = Column(Integer, primary_key=True)
-	sku = Column(String(250))
-	qty = Integer(String(250))
-	order_id = Column(Integer, ForeignKey('order.id'))
-	order = relationship(Order)
+order_lines = Table("order_lines", metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("sku", String(255)),
+    Column("qty", Integer, nullable=False),
+    Column("orderid", String(255))
 
-class Allocation(Base)
+
+def start_mappers():
+	lines_mapper = mapper(model.OrderLine, order_lines)
