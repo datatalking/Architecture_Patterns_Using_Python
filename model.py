aac2043c21afe8bc466c4608d1aa08ee038f52c4
Andrew Schell <paintstone@gmail.com>
2025-02-22 17:58:58 -0800

    added test_saving_batches in tests, will migrate to integrations

--- model.py
+++ model.py
@@ -1,12 +1,24 @@
-#
-import dataclasses
-import typing
-from typing import List
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Optional List, Set
 from datetime import date
 
 
+class OutOfStock(Exception):
+	pass
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+	try:
+		batch = next(b for b in sorted(batches) if b.can_allocate(line))
+		batch.allocate(line)
+		return batch.reference
+	except StopIteration:
+		raise OutOfStock(f"Out of stock for sku")
+
+
 # an immutable dataclass with no behavior
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass(unsafe_hash=True)
 class OrderLine:
 	orderid: str
 	sku:str
@@ -14,16 +26,15 @@ class OrderLine:
 
 
 class Batch:
-	def __init__(self,
-	             ref: str,
-	             sku: str,
-	             qty: int,
-	             eta: typing.Optional[date]):
+	def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
 		self.reference = ref
 		self.sku = sku
 		self.eta = eta
 		self._purchased_quantity = qty
 		self._allocations = set()
+
+	def __repr__(self):
+		return f"<Batch {self.reference}>"
 
 	def __eq__(self, other):
 		if not isinstance(other, Batch):
@@ -58,17 +69,3 @@ class Batch:
 
 	def can_allocate(self, line: OrderLine) ->bool:
 		return self.sku == line.sku and self.available_quantity >= line.qty
-
-
-
-def allocate(line: OrderLine, batches: List[Batch]) -> str:
-	"""
-	
-	:param line:
-	:param batches:
-	:return:
-	"""
-	batch = next(b for b in sorted(batches)
-	             if b.can_allocate(line))
-	batch.allocate(line)
-	return batch.reference
--- test/test_orm.py
+++ test/test_orm.py
@@ -1,8 +1,9 @@
 #
-import model
+from domain import model
+from datetime import date
 
 
-def test_orderline_mapper_can_load(session):
+def test_orderline_mapper_can_load_lines(session):
     """
     test if orderlines can be loaded
     :param session:
@@ -29,8 +30,32 @@ def test_orderline_mapper_can_save_lines(session):
     :return:
     """
     new_line = model.OrderLine("order1", "DECORATIVE-WIDGET", 12)
-    session.add(line)
+    session.add(new_line)
     session.commit()
 
-    rows = list(session.execute('SELECT orderid, sku, qty FROM order_lines"'))
+    rows = list(session.execute('SELECT orderid, sku, qty FROM "order_lines"'))
     assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
+
+
+def test_retrieving_batches(session):
+    session.execute("INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+                    ' VALUES ("batch1", "sku1", 100, null)'
+                    )
+    session.execute("INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+                    ' VALUES ("batch2", "sku2", 200, "2011-04-11")'
+                    )
+    expected = [
+        model.Batch("batch1", "sku1", 100, eta=None),
+        model.Batch("batch2", "sku2", 200, eta=date(2011, 4, 11)),
+    ]
+
+    assert session.query(model.Batch).all() == expected
+
+def test_saving_batches(session):
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    line = model.OrderLine("order1", "sku1", 10)
+    session.add(batch)
+    session.commit()
+    rows = list(session.execute('SELECT orderline_id, batch_id FROM "allocations"'))
+    assert rows == [(line.id, batch.id)
+
